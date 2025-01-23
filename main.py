4262fbfd9dfade4392ac88a6adaada1f96f63671
Po-Wei Chiao <po-wei.chiao@leica-microsystems.com>
2025-01-23 09:30:35 +0800

    update

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ from datetime import datetime, timedelta
 from stock import StockCenter
 from trade import Trade
 from profolio import Profolio
-from simulator import IndexSimulator, EtfSimulator
 
 def dca() -> None:
     """
     Simulates a trading operation using dollar-cost averaging (DCA).
     """
-    codes = ['TQQQ', 'QLD', 'QQQ', 'UPRO', 'SSO', 'VOO']
-    start_time = datetime(2021,1,1)
+    codes = ['TQQQ', 'QQQ', 'UPRO', 'VOO', 'NVDA', 'MSFT', 'AAPL', 'AMZN', 'GOOGL', 'TSLA', 'TSM']
+    start_time = datetime(2024,1,1)
     holding_time = timedelta(days=365*1)
 
     profolio = Profolio(0)
@@ -21,7 +20,7 @@ def dca() -> None:
 
     # Perform DCA trade operation
     for code in codes:
-        trade.dca(code, start_time, end_time, 14, 1600)
+        trade.dca(code, start_time, end_time, 14, 1000)
         # Retrieve holding information
         info = profolio.get_holding_info(code)
     
@@ -33,19 +32,6 @@ def dca() -> None:
         print(f'from {start_time.strftime('%Y-%m-%d')} to {end_time.strftime('%Y-%m-%d')}')
         print(f'holding {info.get_share()} {code} in {round(info.get_average(), 2)}, 'f'profit: {round(info.get_profit_in_percentage(current_time), 2)}%')
         print(f'current price: {round(stockCenter.get_stock_price(code, current_time), 2)} in {current_time.strftime('%Y-%m-%d')}')
-    
-def leveragedSimulator() -> None:
-    start = datetime(2026,1,1)
-    end = datetime(2027,1,1)
-    indexA = IndexSimulator(start, end)
-    
-    leveragedEtf1t = EtfSimulator(indexA, 1).get_etf()
-    leveragedEtf2t = EtfSimulator(indexA, 2).get_etf()
-    leveragedEtf3t = EtfSimulator(indexA, 3).get_etf()
-
-    print(indexA.get_index())
-    print(leveragedEtf1t)
-    print(leveragedEtf2t)
 
 if __name__ == "__main__":
     dca()
